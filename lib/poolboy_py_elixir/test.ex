035bc3aef30606a7ed3890f8bf4244ff95be151c
Abhishek <abhishek.tripathi456@gmail.com>
2022-02-22 22:45:57 +0530

    working poolboy example

--- lib/poolboy_py_elixir/test.ex
+++ lib/poolboy_py_elixir/test.ex
@@ -2,7 +2,7 @@ defmodule PoolboyPyElixir.Test do
   @timeout 60000
 
   def start do
-    1..20
+    1..50
     |> Enum.map(fn i -> async_call_square_root(i) end)
     |> Enum.each(fn task -> await_and_inspect(task) end)
   end
@@ -10,20 +10,19 @@ defmodule PoolboyPyElixir.Test do
   defp async_call_square_root(i) do
     Task.async(fn ->
       :poolboy.transaction(
-        :worker,
+        :square_worker,
         fn pid ->
-
           # Let's wrap the genserver call in a try - catch block. This allows us to trap any exceptions
           # that might be thrown and return the worker back to poolboy in a clean manner. It also allows
           # the programmer to retrieve the error and potentially fix it.
 
-          try do
-            GenServer.call(pid, {:square_root, i})
-          catch
-            e, r ->
-              IO.inspect("poolboy transaction caught error: #{inspect(e)}, #{inspect(r)}")
-              :ok
-          end
+          # try do
+          GenServer.call(pid, {:square_root, i})
+          # catch
+          #   e, r ->
+          #     IO.inspect("poolboy transaction caught error: #{inspect(e)}, #{inspect(r)}")
+          #     :ok
+          # end
         end,
         @timeout
       )
